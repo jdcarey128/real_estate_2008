f5a642ca9c785658bb17d44027e8b3fc67e4cf33
Joshua Carey <jdcarey128@gmail.com>
2020-08-26 09:19:29 -0600

    Write passing methods for area, is_painted?, and paint

--- test/room_test.rb
+++ test/room_test.rb
@@ -4,37 +4,34 @@ require './lib/room'
 
 class RoomTest < Minitest::Test
   def setup
-    @room = Room.new(:bedroom, 10, '13')
     @room1 = Room.new(:bedroom, 10, '13')
     @room2 = Room.new(:living_room, 15, '12')
   end
 
   def test_it_exists
-    assert_instance_of Room, @room
+    assert_instance_of Room, @room1
   end
 
   def test_it_has_a_category
-    skip
 
-    assert_equal :bedroom, @room.category
+    assert_equal :bedroom, @room1.category
   end
 
   def test_it_can_get_area
-    skip
+
     assert_equal 130, @room1.area
     assert_equal 180, @room2.area
   end
 
   def test_it_can_inquire_if_painted
-    skip
 
-    assert_equal false, @room.is_painted?
+    assert_equal false, @room1.is_painted?
   end
 
   def test_paint_method_asserts_paint_inquiry
-    skip
-    @room.paint
-    assert @room.is_painted?
+    
+    @room1.paint
+    assert @room1.is_painted?
   end
 
 end
