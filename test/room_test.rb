4c714e664e698e762035168c8873c5964b60971e
Joshua Carey <jdcarey128@gmail.com>
2020-08-26 09:12:44 -0600

    Refactor tests with setup and add paint assertions

--- test/room_test.rb
+++ test/room_test.rb
@@ -3,22 +3,38 @@ require 'minitest/pride'
 require './lib/room'
 
 class RoomTest < Minitest::Test
+  def setup
+    @room = Room.new(:bedroom, 10, '13')
+    @room1 = Room.new(:bedroom, 10, '13')
+    @room2 = Room.new(:living_room, 15, '12')
+  end
+
   def test_it_exists
-    room = Room.new(:bedroom, 10, '13')
-    assert_instance_of Room, room
+    assert_instance_of Room, @room
   end
 
   def test_it_has_a_category
     skip
-    room = Room.new(:bedroom, 10, '13')
-    assert_equal :bedroom, room.category
+
+    assert_equal :bedroom, @room.category
   end
 
   def test_it_can_get_area
     skip
-    room1 = Room.new(:bedroom, 10, '13')
-    room2 = Room.new(:living_room, 15, '12')
-    assert_equal 130, room1.area
-    assert_equal 180, room2.area
+    assert_equal 130, @room1.area
+    assert_equal 180, @room2.area
   end
+
+  def test_it_can_inquire_if_painted
+    skip
+
+    assert_equal false, @room.is_painted?
+  end
+
+  def test_paint_method_asserts_paint_inquiry
+    skip
+    @room.paint
+    assert @room.is_painted?
+  end
+
 end
